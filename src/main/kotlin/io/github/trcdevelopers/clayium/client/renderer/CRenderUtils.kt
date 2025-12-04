0f9a103342d40bccc984af85aeef6fb6dbcd917e
bqc0n <me@bqc0n.com>
2025-12-04 12:49:25 +0900

    Refactor Rendering (#413)
    * fix: disableTranslucent missing disableBlend()
    
    * fix: don't modify texture2d in enable/disableTranslucent
    
    * feat: rm disableXXX method from CRenderUtils. they are replaced by memory/restore states
    
    * style: fix typo
    
    * feat: rm state tracking from CRenderUtils, use CCL's GlStateTracker instead
    
    * fix: reduce GL call from CaReactor
    
    * style: fix typo
    
    * feat: don't modify Texture2D in enableXray method
    
    * style: Emphasizing that each of the six vertices is specified
    
    ---------
    
    Co-authored-by: soramame0256 <62879324+soramame0256@users.noreply.github.com>

--- src/main/kotlin/io/github/trcdevelopers/clayium/client/renderer/CRenderUtils.kt
+++ src/main/kotlin/io/github/trcdevelopers/clayium/client/renderer/CRenderUtils.kt
@@ -1,13 +1,21 @@
 package io.github.trcdevelopers.clayium.client.renderer
 
+import codechicken.lib.render.state.GlStateTracker
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.renderer.Tessellator
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats
-import org.lwjgl.BufferUtils
+import net.minecraft.util.math.AxisAlignedBB
 import org.lwjgl.opengl.GL11
 
 object CRenderUtils {
+    /**
+     * Disables: lighting, depthMask.
+     * Enables: blend, depth, blendFunc.
+     *
+     * You can use [codechicken.lib.render.state.GlStateTracker] to push/pop GL states around this call.
+     * All the states modified by this method are tracked by GlStateTracker.
+     */
     fun enableTranslucent() {
         GlStateManager.disableLighting()
         GlStateManager.enableBlend()
@@ -17,44 +25,34 @@ object CRenderUtils {
         )
         GlStateManager.enableDepth()
         GlStateManager.depthMask(false)
-
-        GlStateManager.disableTexture2D()
     }
 
-    fun disableTranslucent() {
-        GlStateManager.enableTexture2D()
-        GlStateManager.depthMask(true)
-        GlStateManager.enableDepth()
-    }
-
+    /**
+     * Disables: lighting, cullFace, depth.
+     *
+     * You can use [codechicken.lib.render.state.GlStateTracker] to push/pop GL states around this call.
+     * All the states modified by this method are tracked by GlStateTracker.
+     */
     fun enableXray() {
-        GlStateManager.disableTexture2D()
         GlStateManager.disableLighting()
         GlStateManager.disableCull()
         GlStateManager.disableDepth()
     }
 
-    fun disableXray() {
-        GlStateManager.enableDepth()
-        GlStateManager.enableCull()
-        GlStateManager.enableLighting()
-        GlStateManager.enableTexture2D()
-    }
-
     /**
-     * Draws given text with given color and a semi-transparent black background at a current scale/position.
+     * Draws given text with given colour and a semi-transparent black background at a current scale/position.
      */
     fun renderStringWithBackground(text: String, color: Int) {
         val mc = Minecraft.getMinecraft()
         GlStateManager.pushMatrix()
-        val memory = memoryCurrentStates()
+        GlStateTracker.pushState()
+        GlStateManager.glNormal3f(0.0f, 1.0f, 0.0f)
+        GlStateManager.color(0f, 0f, 0f, 0.5f)
+        GlStateManager.disableTexture2D()
+        this.enableTranslucent()
         run {
-            GlStateManager.glNormal3f(0.0f, 1.0f, 0.0f)
-            val width = mc.fontRenderer.getStringWidth(text) / 2
 
-            GlStateManager.color(0f, 0f, 0f, 0.5f)
-            this.enableTranslucent()
-            GlStateManager.disableTexture2D()
+            val width = mc.fontRenderer.getStringWidth(text) / 2
             val tessellator = Tessellator.getInstance()
             val bufferBuilder = tessellator.buffer
             bufferBuilder.begin(7, DefaultVertexFormats.POSITION)
@@ -63,77 +61,49 @@ object CRenderUtils {
             bufferBuilder.pos(width + 1.0, 8.0, 0.0).endVertex()
             bufferBuilder.pos(width + 1.0, -1.0, 0.0).endVertex()
             tessellator.draw()
+
             GlStateManager.enableTexture2D()
             GlStateManager.depthMask(true)
-
             Minecraft.getMinecraft().fontRenderer.drawString(text, -width, 0, color)
         }
-        this.restoreStates(memory)
+        GlStateTracker.popState()
         GlStateManager.popMatrix()
     }
 
-    fun memoryCurrentStates(): GlStatesInformation {
-        return GlStatesInformation()
-    }
+    fun renderCube(aabb: AxisAlignedBB, r: Float, g: Float, b: Float, a: Float) {
+        val tessellator = Tessellator.getInstance()
+        val bufferBuilder = tessellator.buffer
+        bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR)
 
-    fun restoreStates(states: GlStatesInformation) {
-        GlStateManager.color(states.r, states.g, states.b, states.a)
-        GlStateManager.shadeModel(states.shadeModel)
-        GlStateManager.bindTexture(states.textureId)
-        if (states.lighting) GlStateManager.enableLighting() else GlStateManager.disableLighting()
-        if (states.texture2D) GlStateManager.enableTexture2D() else GlStateManager.disableTexture2D()
-        if (states.alphaTest) GlStateManager.enableAlpha() else GlStateManager.disableAlpha()
-        if (states.depthTest) GlStateManager.enableDepth() else GlStateManager.disableDepth()
-        if (states.depthMask) GlStateManager.depthMask(true) else GlStateManager.depthMask(false)
-        if (states.cullFace) GlStateManager.enableCull() else GlStateManager.disableCull()
-        // Blend
-        if (states.blend) {
-            GlStateManager.enableBlend()
-            GlStateManager.tryBlendFuncSeparate(
-                states.blendSrc, states.blendDst,
-                states.blendSrcAlpha, states.blendDstAlpha
-            )
-        } else {
-            GlStateManager.disableBlend()
-        }
-    }
+        bufferBuilder.pos(aabb.minX, aabb.maxY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.maxY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.minY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.minY, aabb.minZ).color(r, g, b, a).endVertex()
 
-    class GlStatesInformation(
-        val lighting: Boolean = GlStateManager.lightingState.currentState,
-        val texture2D: Boolean = GL11.glIsEnabled(GL11.GL_TEXTURE_2D),
-        val alphaTest: Boolean = GlStateManager.alphaState.alphaTest.currentState,
-        val depthTest: Boolean = GlStateManager.depthState.depthTest.currentState,
-        val depthMask: Boolean = GlStateManager.depthState.maskEnabled,
-        val cullFace: Boolean = GlStateManager.cullState.cullFace.currentState,
-    ) {
+        bufferBuilder.pos(aabb.minX, aabb.minY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.minY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.maxY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.maxY, aabb.maxZ).color(r, g, b, a).endVertex()
 
-        val r: Float
-        val g: Float
-        val b: Float
-        val a: Float
-        val textureId: Int
-        val shadeModel: Int
+        bufferBuilder.pos(aabb.minX, aabb.minY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.minY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.minY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.minY, aabb.maxZ).color(r, g, b, a).endVertex()
 
-        val blend = GlStateManager.blendState.blend.currentState
-        val blendSrc = GlStateManager.blendState.srcFactor
-        val blendSrcAlpha = GlStateManager.blendState.srcFactorAlpha
-        val blendDst = GlStateManager.blendState.dstFactor
-        val blendDstAlpha = GlStateManager.blendState.dstFactorAlpha
+        bufferBuilder.pos(aabb.minX, aabb.maxY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.maxY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.maxY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.maxY, aabb.minZ).color(r, g, b, a).endVertex()
 
-        init {
-            // Minimal size 16.
-            // java.lang.IllegalArgumentException: Number of remaining buffer elements is 4, must be at least 16.
-            // Because at most 16 elements can be returned, a buffer with at least 16 elements is required, regardless of actual returned element count
-            val rgba = BufferUtils.createFloatBuffer(16)
-            GL11.glGetFloat(GL11.GL_CURRENT_COLOR, rgba)
-            r = rgba[0]
-            g = rgba[1]
-            b = rgba[2]
-            a = rgba[3]
+        bufferBuilder.pos(aabb.minX, aabb.minY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.maxY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.maxY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.minX, aabb.minY, aabb.minZ).color(r, g, b, a).endVertex()
 
-            // Use non-buffer version, because buffer version is shifted by 8 bits.
-            this.textureId = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D)
-            this.shadeModel = GL11.glGetInteger(GL11.GL_SHADE_MODEL)
-        }
+        bufferBuilder.pos(aabb.maxX, aabb.minY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.maxY, aabb.minZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.maxY, aabb.maxZ).color(r, g, b, a).endVertex()
+        bufferBuilder.pos(aabb.maxX, aabb.minY, aabb.maxZ).color(r, g, b, a).endVertex()
+        tessellator.draw()
     }
 }
